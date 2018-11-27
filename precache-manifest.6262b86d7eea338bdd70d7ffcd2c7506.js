f161a2c1e01e8617f18a1f7d99d20c43dd54ec53
Vlad Lytvynenko <vlad.lytvynenko12@gmail.com>
2018-11-27 02:46:43 +0200

    Updates

---
+++ precache-manifest.6262b86d7eea338bdd70d7ffcd2c7506.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7016c42ddf7766cdbcb",
+    "url": "/to-do-react/static/js/runtime~main.f7016c42.js"
+  },
+  {
+    "revision": "1ed31ed36e3defd61799",
+    "url": "/to-do-react/static/js/main.1ed31ed3.chunk.js"
+  },
+  {
+    "revision": "bc951937f3a44b7747ac",
+    "url": "/to-do-react/static/js/1.bc951937.chunk.js"
+  },
+  {
+    "revision": "1ed31ed36e3defd61799",
+    "url": "/to-do-react/static/css/main.5d78f770.chunk.css"
+  },
+  {
+    "revision": "95c0006c8a0367ce52c72109917ac656",
+    "url": "/to-do-react/index.html"
+  }
+];
